dc7ae02cd884973fe8ff3f006de760bd91bc299c
Archil Bolkvadze <a.bolkvadze@gmail.com>
2010-01-11 15:45:45 +0400

    Explored Puting Entity Part into region technique

---
+++ Exploration/PartPresenter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Input;
+using Microsoft.Practices.Composite.Presentation.Commands;
+using Microsoft.Practices.Composite.Presentation.Regions;
+using Microsoft.Practices.Unity;
+
+namespace Exploration
+{
+    public class PartPresenter
+    {
+        private readonly IUnityContainer _container;
+        private readonly RegionManager _regionManager;
+        public PartPresenterView View { get; set; }
+        Lazy<DelegateCommand<object>> _addPartCommand;
+
+        public PartPresenter(PartPresenterView view, IUnityContainer container)
+        {
+            _container = container;
+            View = view;
+            View.DataContext = this;
+            _regionManager = new RegionManager();
+            RegionManager.SetRegionManager(View, _regionManager);
+
+            _addPartCommand = new Lazy<DelegateCommand<object>>(() => new DelegateCommand<object>(AddPartExecuted));
+            Action<int> add = (i) =>
+            {
+                var region = _regionManager.Regions["Page1Content" + i];
+                if (region.Views.Count() == 0)
+                {
+                    var partView = _container.Resolve<PartView>();
+
+                    region.Add(partView);
+                    region.Activate(partView);
+                    
+                }
+            };
+
+            add(1);
+            add(2);
+            add(3);
+        }
+
+        private void AddPartExecuted(object o)
+        {
+            _regionManager.Regions["TabRegion"].Add(_container.Resolve<PartView>());
+
+
+
+        }
+
+        public ICommand AddPartCommand
+        {
+            get
+            {
+
+                return _addPartCommand.Value;
+            }
+        }
+    }
+}
--- Exploration/PartView.xaml.cs
+++ Exploration/PartView.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -19,9 +20,27 @@ namespace Exploration
     /// </summary>
     public partial class PartView : UserControl
     {
+        private bool isAnimating = false;
+
         public PartView()
         {
             InitializeComponent();
+            this.Loaded += new RoutedEventHandler(PartView_Loaded);
+        }
+
+        void PartView_Loaded(object sender, RoutedEventArgs e)
+        {
+            StartAnimation();
+        }
+
+        public void StartAnimation()
+        {
+            var storyboard = this.FindResource("Storyboard1") as Storyboard;
+            if (storyboard != null && !isAnimating)
+            {
+                storyboard.Begin();
+                isAnimating = true;
+            }
         }
     }
 }
